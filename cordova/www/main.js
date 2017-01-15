4eeb4b14b3f81c25f16f46fac319dff0573fe922
Ricardo Parro <ricardo.parro@gmail.com>
2017-01-15 22:31:44 +0100

    integrated firebase

--- cordova/www/main.js
+++ cordova/www/main.js
@@ -1,14 +1,22 @@
-var obj = jQuery.parseJSON( '{ "personList": [{ "name": "Ricardo Parro", "image": "ric_thumb.jpg" },{"name":"Caterine", "image": "https://pbs.twimg.com/profile_images/818202376129642496/STERKdy4.jpg"}]}');
 
 
 $(document).ready(function(){
 
-	$.each(obj.personList, function(index, person){
 
-			console.log(person);
-			$('#listImages').append('<img src="' + person.image +'">');
-		
+
+var database = firebase.database();
+
+database.ref('/twitter_users/').once('value').then(function(snapshot) {
+  var user_list = snapshot.val();
+
+  $.each(user_list, function(index, user){
+  	console.log(user);
+  	$('#listImages').append('<img src="' + user.profile_image_url+'">');
 	});
+  // ...
+});
+
+	
 });
 
 
