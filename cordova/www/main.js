6cea1f1159c89a0cea39be714638282ed8f27920
Ricardo Parro <ricardo.parro@gmail.com>
2017-01-16 22:56:35 +0100

    adding a grid to the view

--- cordova/www/main.js
+++ cordova/www/main.js
@@ -4,19 +4,37 @@ $(document).ready(function(){
 
 
 
-var database = firebase.database();
+	var database = firebase.database();
 
-database.ref('/twitter_users/').once('value').then(function(snapshot) {
-  var user_list = snapshot.val();
+	database.ref('/twitter_users/').once('value').then(function(snapshot) {
+	  
+	  var user_list = snapshot.val();
 
-  $.each(user_list, function(index, user){
-  	console.log(user);
-  	$('#listImages').append('<img src="' + user.profile_image_url+'">');
+	  i=0;
+
+//instead of appending the elements I can lay down the elements on the page and then choose what id to append to
+	  $.each(user_list, function(index, user){
+	  	console.log(user);
+		i++;
+		element ='';
+		if(i === 1){
+			element='<div class="ui-grid-b" data-filter="true"><div class="ui-block-a"><img src="' + user.profile_image_url +'">';
+		}
+
+		if(i === 2){
+			element= '<div class="ui-block-b"><img src="' + user.profile_image_url+'"></div>';
+		}
+
+		if(i === 3){
+			element= '<div class="ui-block-c"><img src="' + user.profile_image_url+'"></div>';
+			i=0;
+		}
+
+
+	  	$('#listImages').append(element);
+		});
 	});
-  // ...
 });
 
-	
-});
 
 
