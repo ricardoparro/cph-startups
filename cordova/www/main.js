646ae42453530d420aa5d33d86e3d27dd7957f78
Ricardo Parro <ricardo.parro@gmail.com>
2017-01-17 22:58:41 +0100

    added dynamic profile

--- cordova/www/main.js
+++ cordova/www/main.js
@@ -1,39 +1,43 @@
 
+var Users = {};
 
 $(document).ready(function(){
-
-
-
+console.log("entered doc ready");
 	var database = firebase.database();
 
 	database.ref('/twitter_users/').once('value').then(function(snapshot) {
 	  
 	  var user_list = snapshot.val();
-
+	  Users = user_list;
 	  i=0;
 
 //instead of appending the elements I can lay down the elements on the page and then choose what id to append to
 	  $.each(user_list, function(index, user){
+
 	  	console.log(user);
-		i++;
-		element ='';
-		if(i === 1){
-			element='<div class="ui-grid-b" data-filter="true"><div class="ui-block-a"><img src="' + user.profile_image_url +'">';
-		}
 
-		if(i === 2){
-			element= '<div class="ui-block-b"><img src="' + user.profile_image_url+'"></div>';
-		}
+	  	$('#listImages').append('<a id="profile_'+ index +'" href="#profile"><img src="' + user.profile_image_url+'"></a>');
+	
+	  	$("#profile_"+ index ).click(function(e) {
+	  		
+	  		$('#twitter_url').attr('href', user.url);
+	  		$('#twitter_url').text('@'+user.screen_name);
+	  		$('#name').text(user.name);
+	  		$('#profile_image').attr('src', user.profile_image_url);
+	  		$('#description').text(user.description);
+        	e.preventDefault();
+        	
+        	window.location.href = "#profile";
 
-		if(i === 3){
-			element= '<div class="ui-block-c"><img src="' + user.profile_image_url+'"></div>';
-			i=0;
-		}
 
-
-	  	$('#listImages').append(element);
+    
+    //    	return false;  
+    	});
 		});
+
 	});
+
+	
 });
 
 
