774b36469a3c4385cd902e944f3b3566721c5f15
Ricardo Parro <ricardo.parro@gmail.com>
2017-01-18 23:36:56 +0100

    changes and deployment preparation

---
+++ cordova/CPH HSTL/CPH HSTL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPH HSTL
+//
+//  Created by Ricardo Parro on 18/01/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ cordova/CPH HSTL/CPH HSTLTests/CPH_HSTLTests.swift
@@ -0,0 +1,36 @@
+//
+//  CPH_HSTLTests.swift
+//  CPH HSTLTests
+//
+//  Created by Ricardo Parro on 18/01/17.
+//
+//
+
+import XCTest
+@testable import CPH_HSTL
+
+class CPH_HSTLTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
