2dc2a4f93bd9b933cf63dafd4e19a99e525bc43b
Ricardo Parro <ricardo.parro@gmail.com>
2017-01-26 22:22:39 +0100

    improved css and added loading

--- cordova/platforms/ios/www/main.js
+++ cordova/platforms/ios/www/main.js
@@ -1,7 +1,25 @@
 
 var Users = {};
 
+
 $(document).ready(function(){
+
+//loading
+	var $this = $( this ),
+	    theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+	    msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
+	    textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+	    textonly = !!$this.jqmData( "textonly" );
+	    html = $this.jqmData( "html" ) || "";
+	$.mobile.loading( "show", {
+	        text: msgText,
+	        textVisible: textVisible,
+	        theme: theme,
+	        textonly: textonly,
+	        html: html
+	});
+
+
 
 	var database = firebase.database();
 
@@ -31,9 +49,11 @@ $(document).ready(function(){
 
     	});
 		});
-
+	//hide loading  
+	$.mobile.loading( "hide" );
 	});
 });
+
 
 
 
